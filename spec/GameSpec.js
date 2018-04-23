a90ab0dabd59a4b0d6e985c2f89736f29ecad40c
Terry Allitt <tallitt88@gmail.com>
2018-04-23 19:21:16 +0100

    :white_check_mark: Game has a frame index that starts at 1

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -12,6 +12,10 @@ describe("Game", function() {
       expect(game.frames).toEqual([]);
     });
 
+    it("should have a frame index that starts at 1", function() {
+      expect(game.frameIndex).toEqual(1);
+    });
+
     it("should have a maximum of 10 frames", function() {
       expect(game._MAX_FRAMES).toEqual(10);
     });
--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 function Game() {
   this.frames = [];
   this.currentFrame = new Frame();
+  this.frameIndex = 1;
   this._MAX_FRAMES = 10;
 }
